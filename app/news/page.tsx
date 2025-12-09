243592b3a983390589acf7bdfedf1384b2f72bc7
Daisuke Ohnuma <dfksoundlab@gmail.com>
2025-12-10 00:20:05 +0900

    Update page.tsx

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,16 +1,21 @@
 import { getEventList } from '@/app/_libs/microcms';
-import { NEWS_LIST_LIMIT } from '@/app/_constants';
-import NewsList from '@/app/_components/NewsList';
+// import { NEWS_LIST_LIMIT } from '@/app/_constants'; // 読み込む定数名を合わせるか、
+const EVENT_LIST_LIMIT = 10; // ← 分かりやすいようにここで定義するめう
+
+// 🚨 読み込む部品の名前を EventList に変えるめう！
+import EventList from '@/app/_components/NewsList'; 
 import Pagination from '@/app/_components/Pagination';
 
 export default async function Page() {
   const data = await getEventList({
-    limit: NEWS_LIST_LIMIT,
+    limit: EVENT_LIST_LIMIT, // 🚨 定数名を修正
   });
   return (
     <>
-      <NewsList articles={data.contents} />
-      <Pagination totalCount={data.totalCount} basePath="/news" />
+      {/* 🚨 コンポーネントの名前も EventList に変えるめう！ */}
+      <EventList articles={data.contents} />
+      {/* このページは1ページ目だから basePath はなくても動くことが多いめうが、次のページへのリンクを張るなら必要めう */}
+      <Pagination totalCount={data.totalCount} basePath="/schedule" /> 
     </>
   );
 }
